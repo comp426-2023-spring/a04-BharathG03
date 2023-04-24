f12e3f86c91a083d1701270c160fd2ee0dd783a8
Bharadwaj Gadiyaram <bharat.gadiyaram@gmail.com>
2023-04-24 16:44:12 -0400

    Default port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { rps, rpsls } from './lib/rpsls.js';
 
 let argv = minimist(process.argv.slice(2));
 const app = express();
-const port = argv.port;
+const port = argv.port || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
