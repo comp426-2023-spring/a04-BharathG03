91ca768e7b4a79b0f4e563cf6197486011c220d7
Bharadwaj Gadiyaram <bharat.gadiyaram@gmail.com>
2023-04-24 15:21:52 -0400

    Created app and set up port

--- server.js
+++ server.js
@@ -3,3 +3,7 @@
 import express from 'express'
 import minimist from 'minimist'
 import { rps, rpsls } from './lib/rpsls.js';
+
+let argv = minimist(process.argv.slice(2));
+const app = express();
+const port = argv.port;
