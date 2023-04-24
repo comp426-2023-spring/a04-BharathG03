d56b3d1a2a5c6c53f93925eb08ae94c506a17699
Bharadwaj Gadiyaram <bharat.gadiyaram@gmail.com>
2023-04-24 15:35:19 -0400

    /app request code

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ import { rps, rpsls } from './lib/rpsls.js';
 let argv = minimist(process.argv.slice(2));
 const app = express();
 const port = argv.port;
+
+app.use.json();
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/app', (req, res) => {
+    res.status(200).send('200 OK');
+});
